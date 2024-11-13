bfc830ec52898433703233a43265840ac55845d7
Kewal Uchiha <kewaltamang73@gmail.com>
2024-11-13 12:44:09 +0100

    Webpack

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App";
 import "./index.css";
 
+// ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // Match .jsx and .js files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: "./dist",
+    port: 3000,
+  },
+};
