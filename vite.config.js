fd101c69688a4018b4e504212fd1af4cf77f5b69
Kewal Uchiha <kewaltamang73@gmail.com>
2024-11-13 20:19:02 +0100

    Install babel

--- vite.config.js
+++ vite.config.js
@@ -3,40 +3,14 @@ import react from "@vitejs/plugin-react";
 
 const someValue = process.env.SOME_ENV_VARIABLE;
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   npm,
-//   assetsInclude: ["**/*.glb"],
-// });
-
 export default defineConfig({
   server: {
     port: 3000,
   },
 
   plugins: [react()],
-  base: "/vite-deploy/",
   assetsInclude: ["**/*.glb"],
+  build: {
+    outDir: "dist",
+  },
 });
-
-// import { resolve } from "path";
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   npm,
-//   assetsInclude: ["**/*.glb"],
-
-//   build: {
-//     lib: {
-//       entry: resolve(__dirname, "src/index.ts"),
-//       name: "myLib",
-//       fileName: "myLib",
-//     },
-//     rollupOptions: {
-//       external: [/^node:\w+/], // <-- ignores all 'node:*'
-//     },
-//   },
-// });
